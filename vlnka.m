802066fa36cde1c56fc7e3b725339be7ecfb3462
zdenyhraz <hrazdira.zdenek@gmail.com>
2020-05-28 18:13:22 +0200

    minor

--- vlnka.m
+++ vlnka.m
@@ -6,7 +6,8 @@ clc;
 k=1*10^7;%1*10^7
 f=1*10^(-1);%1*10^(-1)
 a=1*10^(-2);%1*10^(-2)
-dz=4*10^(-4);%3*10^(-4)
+dz=5*10^(-4);%3*10^(-4)
+dz1D=5*10^(-4);
 drho=1.5*10^(-5);%1.5*10^(-5)
 
 N=101;
@@ -32,10 +33,10 @@ end
 %============================ 1D ============================
 
 for it=1:N1D
-    z=f-dz+(it-1)/(N1D-1)*2*dz;
+    z=f-dz1D+(it-1)/(N1D-1)*2*dz1D;
     zi(it)=1000*(z-f);%[mm]
     
-    S1D1(it)=(2*i)/(k*((a/f)^2));
+    S1D1(it)=1/(-i*k/2*(a/f)^2*(1-3/4*(a/f)^2+5/8*(a/f)^4-35/64*(a/f)^6));
     S1D2(it)=z/(sqrt(z^2+a^2));
     S1D3(it)=exp(i*k*(sqrt(z^2+a^2)-sqrt(f^2+a^2)))/(sqrt(z^2+a^2)-sqrt(f^2+a^2));
     S1D4(it)=exp(i*k*(z-f))/(z-f);
@@ -52,7 +53,7 @@ parfor iz=1:N
         rho=0-drho+(irho-1)/(N-1)*2*drho;
         phi=0;
         
-        S1=-(i*k*z)/(2*pi);
+        S1=-i*k*z/(2*pi);
         S2=@(r) r.*(exp(-i*k*sqrt(f^2+r.^2)))/(sqrt(f^2+r.^2));
         S3=@(p,r) (exp(i*k*sqrt(z^2+rho^2+r.^2-2*rho*r.*cos(p-phi)))/(z^2+rho^2+r.^2-2*rho*r.*cos(p-phi)))*(1+i./(k*sqrt(z^2+rho^2+r.^2-2*rho*r.*cos(p-phi))));
         I1=@(r) integral(@(p) S3(p,r),0,2*pi,'AbsTol',1e-0,'ArrayValued',true);
@@ -79,30 +80,40 @@ fprintf('zfidx=%d\n',zfidx);
 %============================ FIGURES ============================
 fntsz=14;
 
+%1D inteenzita
 figure('Position',[100 300 800 600])
 plot(zi,INTENSITY1D);
 title('$\psi(0,-,z)$ Osova relativni intenzita pro f='+compose("%.2g",f)+', a='+compose("%.2g",a)+', k='+compose("%.2g",k),'fontsize',fntsz,'Interpreter','latex')
 xlabel('$z-f$[mm]','Interpreter','latex')
-xlim([-1000*dz,1000*dz])
+xlim([-1000*dz1D,1000*dz1D])
 
+%rez podel z
 figure('Position',[100 300 800 600])
 plot(x,INTENSITYAXZ);
 title('$\psi(0,-,z)$ Rez relativni intenzity ve smeru osy $z$ pro $\rho=0$ pro f='+compose("%.2g",f)+', a='+compose("%.2g",a)+', k='+compose("%.2g",k),'fontsize',fntsz,'Interpreter','latex')
 xlabel('$z-f$[mm]','Interpreter','latex')
-xlim([-1000*dz,1000*dz])
 
+%rez podel z zoom
 figure('Position',[100 300 800 600])
-semilogy(x,INTENSITYAXZ);
-title('$\psi(0,-,z)$ (log) Rez relativni intenzity ve smeru osy $z$ pro $\rho=0$ pro f='+compose("%.2g",f)+', a='+compose("%.2g",a)+', k='+compose("%.2g",k),'fontsize',fntsz,'Interpreter','latex')
+plot(x,INTENSITYAXZ);
+title('$\psi(0,-,z)$ Rez relativni intenzity ve smeru osy $z$ pro $\rho=0$ pro f='+compose("%.2g",f)+', a='+compose("%.2g",a)+', k='+compose("%.2g",k),'fontsize',fntsz,'Interpreter','latex')
 xlabel('$z-f$[mm]','Interpreter','latex')
-xlim([-1000*dz,1000*dz])
-ylim([min(INTENSITYAXZ),1])
+ylim([0,0.1])
 
+%rez podel rho
 figure('Position',[100 300 800 600])
 plot(y,INTENSITYAXR);
 title('$\psi(\rho,-,f)$ Rez relativni intenzity ve smeru osy $\rho$ pro $z=f$ pro f='+compose("%.2g",f)+', a='+compose("%.2g",a)+', k='+compose("%.2g",k),'fontsize',fntsz,'Interpreter','latex')
 xlabel('$\rho$[mm]','Interpreter','latex')
 
+%rez podel rho zoom
+figure('Position',[100 300 800 600])
+plot(y,INTENSITYAXR);
+title('$\psi(\rho,-,f)$ Rez relativni intenzity ve smeru osy $\rho$ pro $z=f$ pro f='+compose("%.2g",f)+', a='+compose("%.2g",a)+', k='+compose("%.2g",k),'fontsize',fntsz,'Interpreter','latex')
+xlabel('$\rho$[mm]','Interpreter','latex')
+ylim([0,0.05])
+
+%contour intensity
 figure('Position',[100 300 800 600])
 contour(X,Y,INTENSITY,[0,0.0001,0.001,0.002,0.003,0.005,0.01,0.02,0.03,0.05,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1],'ShowText','on');
 title('$\psi(\rho,-,z)$ Relativni intenzita pro f='+compose("%.2g",f)+', a='+compose("%.2g",a)+', k='+compose("%.2g",k),'fontsize',fntsz,'Interpreter','latex')
